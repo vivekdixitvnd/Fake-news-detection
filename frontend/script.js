f450ef8e96e9ec06065f0484d77b972a84c28401
Vivek Kumar Dixit <98418912+vivekdixitvnd@users.noreply.github.com>
2025-04-28 02:14:54 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 async function checkNews() {
   const newsText = document.getElementById('newsInput').value;
 
-  const res = await fetch('https://fake-news-detection-dii2.onrender.com/predict', {
+  const res = await fetch('https://fake-news-detection-71hi.onrender.com/predict', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
